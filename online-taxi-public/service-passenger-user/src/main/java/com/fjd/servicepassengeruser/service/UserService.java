282d2f08193cc1827122644c87ffc6c391e5218d
fanjidong <fanjidong@autelrobotics.cn>
2023-03-05 18:24:25 +0800

    mod = 插入用户

--- online-taxi-public/service-passenger-user/src/main/java/com/fjd/servicepassengeruser/service/UserService.java
+++ online-taxi-public/service-passenger-user/src/main/java/com/fjd/servicepassengeruser/service/UserService.java
@@ -6,6 +6,8 @@ import com.fjd.servicepassengeruser.mapper.PassengerUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.PolicySpi;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,9 +29,21 @@ public class UserService {
         Map<String, Object> map = new HashMap<>();
         map.put("passenger_phone", passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers.size() == 0 ? "无记录" : passengerUsers.get(0).getPassengerName());
 
         //判断用户信息是否存在
+        if(passengerUsers.size() == 0){
+            //如果不存在，插入用户信息
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setPassengerName("张三"); //用户名是由app自己生产的，或者用户自己设置
+            passengerUser.setPassengerGender((byte) 0);
+
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+
+            passengerUserMapper.insert(passengerUser);
+        }
 
         //如果不存在，插入用户信息
 
