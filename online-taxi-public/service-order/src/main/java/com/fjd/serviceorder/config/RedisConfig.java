6d594abd030c33f658d8188905c9c0c2b77715a3
fanjidong <fanjidong@autelrobotics.cn>
2023-04-02 22:38:54 +0800

    mod = redis代码优化

--- online-taxi-public/service-order/src/main/java/com/fjd/serviceorder/config/RedisConfig.java
+++ online-taxi-public/service-order/src/main/java/com/fjd/serviceorder/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.fjd.serviceorder.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -16,10 +17,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisConfig {
 
+    private String potocol = "redis://";
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private String redisPort;
+
     @Bean
     public RedissonClient redisClient(){
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://10.250.4.7:6389").setDatabase(0);
+        config.useSingleServer().setAddress(potocol + redisHost + ":" + redisPort).setDatabase(0);
         return Redisson.create(config);
     }
 }
