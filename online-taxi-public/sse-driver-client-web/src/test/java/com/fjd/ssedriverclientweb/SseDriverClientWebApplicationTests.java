d020232266f926b0a20f2a20d280e4c6840c1e76
fanjidong <fanjidong@autelrobotics.cn>
2023-04-02 23:05:40 +0800

    mod = 模拟司机客户端的开发

---
+++ online-taxi-public/sse-driver-client-web/src/test/java/com/fjd/ssedriverclientweb/SseDriverClientWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjd.ssedriverclientweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseDriverClientWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
